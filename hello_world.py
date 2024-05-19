36524b98711a1bfd4f95984c2b35039115913f8c
xcematin <xcematin@outlook.com>
2024-05-19 23:55:23 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,17 @@
 from prefect import flow
-
-
+import requests
 @flow(log_prints=True)
-def hello_world(name: str = "world", goodbye: bool = False):
-    print(f"Hello {name} from Prefect! 🤗")
-
-    if goodbye:
-        print(f"Goodbye {name}!")
-
+def hello_world(name: str,rowId: str,server_url: str):
+    if name and rowId and server_url:
+        print(f"Update {name} data from Server! 🤗")
+        response = requests.post(server_url,json={"name": name, "rowId": rowId})
+        if response.status_code == 200:
+            print(f"Data updated successfully! 🤗")
+        else:
+            raise ValueError(f"Error while updating data! 😔")
+    else:
+        raise ValueError("Name and rowId and server_url are required!")
+    
 
 if __name__ == "__main__":
     hello_world.from_source(
